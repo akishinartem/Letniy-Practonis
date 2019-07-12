2f7c0021286f977381dbe1d5e037382a5eb84aba
Adidasik <43314978+akishinartem@users.noreply.github.com>
2019-07-13 02:47:50 +0300

    Тело цикла основной программы

--- 2018/main.c
+++ 2018/main.c
@@ -1,5 +1,3 @@
-#define _CRT_SECURE_NO_WARNINGS
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,22 +7,17 @@ char massive[LEN]
 int count;
 int strings;
 
-
-char cmp_by_name(const void * a, const void * b)
-{
+char cmp_by_name(const void * a, const void * b) {
 	return strcmp(((struct travi*)a)->name_r, ((struct travi*)b)->name_r);
 }
 
-int main()
-{
+int main() {
 	char str1[LEN];
 	FILE *file1;
 	file1 = fopen("C:\\Program Files\\text.txt", "r+");
 	
-	if (file1 != NULL)
-	{
-		while (feof(file1) == 0 || count > 10)
-		{
+	if (file1 != NULL) {
+		while (feof(file1) == 0 || count > 10) {
 			fscanf(file1, "%s", str1);
 			strcpy(strings[count++], str1);
 		}
@@ -32,19 +25,15 @@ int main()
 		struct travi *p;
 		qsort(p, str1, sizeof(struct travi), cmp_by_name);
 	}
-		else
-		{
-			fprintf(stderr, "ERROR: file wasn't opened!\n");
-			return 1;
-		}
+	else {
+		fprintf(stderr, "ERROR: file wasn't opened!\n");
+		return 1;
+	}
 
-
-	for (i = 0; i < n; i++)
-	{
+	for (i = 0; i < n; i++) {
 		printf("%s\n", &p[i].FIO);
 		fprintf(file1, "%s\n", &p[i].FIO);
 	}
-	
 	fclose(file1);
 	free(p);
 	return 0;
